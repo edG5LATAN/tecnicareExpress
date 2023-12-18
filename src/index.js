cf9f4439cd834990724ad02c9cc64e3e1b8f0c83
edG5LATAN <136633393+edG5LATAN@users.noreply.github.com>
2023-12-18 15:34:30 -0600

    Add files via upload
    actualizacion de cors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import Express from 'express'
 import router from './router/RouterTecnicare.js'
-import cors from 'cors'
-
 
 import { PORT } from './config.js'
 
 const app = Express()
-app.use(cors())
-
 app.use(Express.json())
-
 app.use(router)
 
 app.use((req,res,next)=>{
