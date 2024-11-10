59f173d3088bbf092952aaba7d556355e26e77f4
edG5LATAN <136633393+edG5LATAN@users.noreply.github.com>
2024-11-09 20:52:47 -0600

    Update config.js
    .env

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ console.log(process.env.PORT)
 
 export const PORT= process.env.PORT||3000
 export const DB_USER= process.env.DB_USER||'root'
-export const DB_PASSWORD= process.env.DB_PASSWORD||'ramnstein'
-export const DB_DATABASE= process.env.DB_DATABASE||'tecnicare'
+export const DB_PASSWORD= process.env.DB_PASSWORD||'password'
+export const DB_DATABASE= process.env.DB_DATABASE||'basedatos'
 export const DB_HOST= process.env.DB_HOST||'localhost'
 export const DB_PORT= process.env.DB_PORT||3306
